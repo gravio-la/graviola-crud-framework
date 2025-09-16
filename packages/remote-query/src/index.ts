cec36e7a542b2278568db1ffdd0ec3338575be71
Winzlieb <winzlieb@c3d2.de>
2025-09-16 11:02:05 +0200

    refactor: remove remoteSparqlQuery module

--- packages/remote-query/src/index.ts
+++ packages/remote-query/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./remoteAllegro";
-export * from "./remoteSparqlQuery";
 export * from "./remoteOxigraph";
 export * from "./remoteQlever";
 export * from "./getSPARQLFlavour";
 export * from "./workerProvider";
+export * from "./authHelpers";
